25867b92ecc1df5512065dedfc1c2f60f201f36c
Anthony Rivers <81377656+rememberanthonyrivers@users.noreply.github.com>
2025-01-19 23:24:42 -0500

    feat: new scatter plot script added

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,9 +3,20 @@
 
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+input_values= [1, 2, 3, 4, 5]
+squares= [1, 4, 9, 16, 25]
+plt.plot(input_values, squares, linewidth=5)
 
-plt.plot(squares)
+# set chart title and label axes .
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels .
+plt.tick_params(axis="both", labelsize=14)
+
+
+# plt.plot(squares)
 # .show() displays the graph in a meaningful way
 # opens matplotlib’s viewer and displays the plot
 plt.show()
---
+++ scatter_squares.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+# x and y values of the scatter plot graphic
+plt.scatter(2,4)
+plt.show()
